3e637e24c3055492edb85d64399db22605a14290
marb <46437892+marb17@users.noreply.github.com>
2025-01-25 18:59:39 +0800

    Delete rebuilt slc project directory

--- rebuilt slc project/js/static.js
+++
@@ -1,9 +0,0 @@
-fetch('../data/info.json')
-    .then(response => response.json())
-    .then(info => {
-        const hotelName = info.name;
-        const avarooms = info.maxrooms - (info.customers).length; /*replace*/
-
-        document.getElementById('hotel-name').textContent = hotelName
-        document.getElementById('avaliable-rooms').textContent = avarooms /*replace*/
-    })
