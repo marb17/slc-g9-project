2e5a7ad615b6bcc8d7a1a4639399f1f1aebd76ca
marb <46437892+marb17@users.noreply.github.com>
2025-02-06 22:16:22 +0800

    Add files via upload

--- js/newcustomer.js
+++ js/newcustomer.js
@@ -4,5 +4,30 @@ function getValues() {
     const duration = document.getElementById('duration').value
     const payment = document.getElementById('payment').value
     const date = document.getElementById('arrival').value
+}
+
+function populateRoomNumberDropdown(data) {
+    const elementRoom = document.getElementById("room")
+
+    const defaultOption = document.createElement("option")
+    defaultOption.value = ""
+    defaultOption.textContent = "Select a Room"
+    defaultOption.disabled = true
+    defaultOption.selected = true
+    elementRoom.appendChild(defaultOption)
+
+    data.forEach(roomname => {
+       const option = document.createElement("option")
+        option.value = roomname
+        option.textContent = roomname
+                
+        elementRoom.appendChild(option)
+    });
 
 }
+
+fetch('../../data/info.json')
+    .then(response => response.json())
+    .then(info => {
+        populateRoomNumberDropdown(info.rooms)
+    })
--- js/static.js
+++ js/static.js
@@ -1,9 +1,31 @@
-fetch('../data/info.json')
+fetch('../../data/info.json')
     .then(response => response.json())
     .then(info => {
         const hotelName = info.name;
         const avarooms = info.maxrooms - (info.customers).length; /*replace*/
 
         document.getElementById('hotel-name').textContent = hotelName
-        document.getElementById('avaliable-rooms').textContent = avarooms /*replace*/
+        try {
+            document.getElementById('avaliable-rooms').textContent = avarooms /*replace*/
+        } catch (error) {
+            console.log("its a different page dw")   
+        }
+
+        /*table population*/
+        function populateTable(data) {
+            const tableBody = document.querySelector("#customer-table")
+            
+            data.forEach(rowdata => {
+                const row = document.createElement("tr")
+                
+                Object.values(rowdata).forEach(value => {
+                    const cell = document.createElement("td")
+                    cell.textContent = value
+                    row.appendChild(cell)
+                });
+                tableBody.appendChild(row)
+            });
+        }
+        
+        populateTable(info.customers)
     })
